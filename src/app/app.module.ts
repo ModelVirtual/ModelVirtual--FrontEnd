ba1a4b49df8c78903608ae17a4073ae9e2932903
Gerardo Gabriel Caldas Díaz <u201512034@upc.edu.pe>
2022-05-06 17:35:07 -0500

    insert shop list
    Shoplist update start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,44 +3,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {MatDividerModule} from "@angular/material/divider";
-import {MatIconModule} from "@angular/material/icon";
+import  {MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './components/login/login.component';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotYourPasswordComponent } from './components/forgot-your-password/forgot-your-password.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { AccountComponent } from './components/account/account.component';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatMenuModule} from "@angular/material/menu";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatMenuModule} from "@angular/material/menu";
 import { EditPersonalDataComponent } from './components/edit-personal-data/edit-personal-data.component';
-import {DeletedFavoritesDialogComponent} from './components/deleted-favorites-dialog/deleted-favorites-dialog.component';
+import { DeletedFavoritesDialogComponent } from './components/deleted-favorites-dialog/deleted-favorites-dialog.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
-import {ProductsComponent} from "./components/products/products.component";
-import {ProductDetailsComponent} from "./components/product-details/product-details.component";
-import {HttpClientModule} from "@angular/common/http";
-import {MatCardModule} from "@angular/material/card";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {ProductService} from "./services/product.service";
+import { ProductsComponent } from "./components/products/products.component";
+import { ProductDetailsComponent } from "./components/product-details/product-details.component";
+import { HttpClientModule } from "@angular/common/http";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { ProductService } from "./services/product.service";
 import { AddedFavoritesDialogComponent } from './components/added-favorites-dialog/added-favorites-dialog.component';
 import { IsLoginComponent } from './components/is-login/is-login.component';
+import { ShoplistComponent } from "./components/shoplist/shoplist.component";
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: '', component: LoginComponent },
-  { path: 'forgotYourPassword', component: ForgotYourPasswordComponent},
-  { path: 'home/:id', component: HomeComponent},
-  { path: 'account/:id', component: AccountComponent},
-  { path: 'account/edit-data/:id', component: EditPersonalDataComponent},
-  { path: 'product/:id', component: ProductsComponent},
-  {path:'product-details/:id',component:ProductDetailsComponent},
-  { path: 'favorites', component: FavoritesComponent}
+  { path: 'forgotYourPassword', component: ForgotYourPasswordComponent },
+  { path: 'home/:id', component: HomeComponent },
+  { path: 'account/:id', component: AccountComponent },
+  { path: 'account/edit-data/:id', component: EditPersonalDataComponent },
+  { path: 'product/:id', component: ProductsComponent },
+  { path: 'product-details/:id',component:ProductDetailsComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'shoplist', component: ShoplistComponent}
 ];
 
 @NgModule({
@@ -57,8 +59,9 @@ const routes: Routes = [
     ProductsComponent,
     ProductDetailsComponent,
     FavoritesComponent,
-     AddedFavoritesDialogComponent,
-     IsLoginComponent
+    AddedFavoritesDialogComponent,
+    IsLoginComponent,
+    ShoplistComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/shoplist/shoplist.component.ts
+++ src/app/components/shoplist/shoplist.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Product} from "../../interfaces/product.interface";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
+import {MatDialog} from "@angular/material/dialog";
+import {FavoriteService} from "../../services/favorite.service";
+import {DeletedFavoritesDialogComponent} from "../deleted-favorites-dialog/deleted-favorites-dialog.component";
 
 @Component({
   selector: 'app-shoplist',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoplistComponent implements OnInit {
 
-  constructor() { }
+  filtertext:string;
+  favoriteData: Product;
+  dataSource: MatTableDataSource<any>;
+  displayedColumns: string[];
 
+  /*@ViewChild(MatPaginator, {static: true})
+  paginator!: MatPaginator;*/
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  constructor(public dialog:MatDialog, private favoriteService:FavoriteService) {
+    this.filtertext="Last visit";
+    this.displayedColumns = ['id', 'name', 'price', 'brand', 'image', 'size'];
+    this.favoriteData = {} as Product;
+    this.dataSource = new MatTableDataSource<any>();
+  }
   ngOnInit(): void {
+    this.getAllFavorites();
+    //this.dataSource.paginator=this.paginator;
+  }
+  getAllFavorites(): void{
+    this.favoriteService.getAll().subscribe((response: any)=>{
+      this.dataSource.data = response;
+      console.log(this.dataSource.data);
+    });
+  }
+
+  deleteFavorite(id: number): void {
+    this.favoriteService.delete(id).subscribe(() => {
+      console.log(`Deleting favorite with id: ${id}`);
+      this.dataSource.data = this.dataSource.data.filter((o: Product) => {
+        return o.id !== id ? o : false;
+      });
+      console.log(this.dataSource.data);
+    })
+    const dialogRef=this.dialog.open(DeletedFavoritesDialogComponent);
+  }
+  filterLastVisit():void {
+    this.filtertext="Last visit";
+  }
+  filterLastWeek(): void {
+    this.filtertext="Last week";
+  }
+  filterLastMonth(): void {
+    this.filtertext="Last month";
+  }
+  filterAll(): void {
+    this.filtertext="All";
   }
 
 }
