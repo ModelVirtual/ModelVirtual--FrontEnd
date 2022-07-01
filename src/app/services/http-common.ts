675409f572e7f9dfee951112e6597a15c4b5fe8c
raquel686 <u201914478@upc.edu.pe>
2022-07-01 18:54:31 -0500

    linkchange

--- src/app/services/http-common.ts
+++ src/app/services/http-common.ts
@@ -1,4 +1,1 @@
-export const apiLink = "http://localhost:8080/api/v1";
-//export const jsonServerLink = "https://json-modelvirtual.azurewebsites.net";
-
-//https://app-modelvirtual--backend-220613181745.azurewebsites.net/api/v1
+export const apiLink = "https://app-modelvirtual--backend-220613181745.azurewebsites.net/api/v1";
